6142ad708269ce1eac5b004d097cfce41c423dbd
bgj424 <bgj424@myy.haaga-helia.fi>
2023-01-12 14:43:07 +0200

    Added e2e test for markers

--- bikeapp-front/cypress/e2e/map_spec.cy.js
+++ bikeapp-front/cypress/e2e/map_spec.cy.js
@@ -1,8 +1,12 @@
 describe('Test select station on map', () => {
-    it('selects station marker on the map', () => {
+    it('hovers and selects station marker on the map', () => {
         cy.visit('http://localhost:3000');
         cy.get('[data-cy="map-container"]')
-            .find('.pigeon-click-block > svg > g').first().click({force: true});
+            .find('.pigeon-click-block > svg > g').first().then($marker => {
+                cy.wrap($marker).trigger('mouseover', {force: true})
+                cy.get('[data-cy="map-container"]').find('.pigeon-click-block > div').should('be.visible');
+                cy.wrap($marker).click({force: true});
+            });
         cy.get('[data-cy="current-journey"]').children('span').should('not.contain', 'Click');
         cy.get('[data-cy="map-container"]').find('.pigeon-click-block').should('have.class', 'active');
     })
